9596b70f159a6c97c74317e68a7743b4074ef41d
Dojun Park <dojun.parkk@gmail.com>
2020-03-25 17:44:25 +0900

    add dynamic memory

---
+++ 51_동적메모리로 알파벳 출력하기.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char *pc = NULL;
+	int i = 0;
+	pc = (char *)malloc(100 * sizeof(char));
+	
+	if(pc == NULL)
+	{
+		printf("동적메모리 할당에 실패했습니다\n");
+		exit(1);
+	}
+	// pc가 가리키는 포인터를 1씩 증가시키며 알파벳 소문자를 삽입합니다.
+	for(i = 0; i <26; i++) 
+	{
+		*(pc + i) = 'a' + i;
+	}
+	// 아스키코드에서 0는 NULL을 의미함 
+	*(pc + i) = 0;
+	
+	printf("%s\n", pc);
+	free(pc);
+	
+	return 0;
+}
